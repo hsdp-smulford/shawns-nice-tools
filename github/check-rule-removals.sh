fb8826cc6f2c9cf2ad4e479def486f077f0820a0
hsp-smulford <48554759+hsdp-smulford@users.noreply.github.com>
2024-07-15 09:45:47 -0400

    improved handling of regions.

---
+++ github/check-rule-removals.sh
@@ -0,0 +1,6 @@
+#!/opt/homebrew/bin/bash -e
+
+# Confirm that all de-centralized config rule evaluations have been removed where expected.
+
+
+
--- github/update-compliance-acct.sh
+++ github/update-compliance-acct.sh
@@ -9,14 +9,14 @@ ENVIRONMENT="$1"; shift
 
 # Function to display the help message
 function display_help() {
-    echo "Usage: $0 [dev|prod] -r regions"
+    echo "Usage: $0 [dev|prod] -r|--regions regions"
     echo
     echo "This script is used to update the compliance account in the AWS Control Tower"
     echo "environment for organization config rules."
     echo
     echo "Arguments:"
     echo "  dev|prod:  The environment to update the compliance account for."
-    echo "  -r regions:  The regions to enable the config rules in, separated by commas; default is all regions."
+    echo "  -r|--regions regions:  The regions to enable the config rules in, separated by commas; default is all regions."
     echo "  -h:  Display this help message."
     exit 1
 }
@@ -26,13 +26,24 @@ if [ "$1" == "-h" ]; then
     display_help
 fi
 
+# Process remaining command-line arguments
+while [[ "$#" -gt 0 ]]; do
+    case $1 in
+        -r|--regions) REGIONS="$2"; shift ;;
+        *) echo "Unknown parameter passed: $1"; display_help ;;
+    esac
+    shift
+done
+
 # Determine the account ID based on the environment
 case $ENVIRONMENT in
     dev)
+        echo "[$(date)]; Updating compliance account for the development environment."
         ACCOUNT_ID=767397720858
         BRANCH=develop
         ;;
     prod)
+        echo "[$(date)]; Updating compliance account for the production environment."
         ACCOUNT_ID=891377304704
         BRANCH=main
         ;;
@@ -41,6 +52,9 @@ case $ENVIRONMENT in
         display_help
         ;;
 esac
+
+echo "[$(date)]; Compliance Account ID: $ACCOUNT_ID"
+echo "[$(date)]; Target Branch: $BRANCH"
 
 # Check if REGIONS is provided and not empty
 if [[ -n "$REGIONS" ]]; then
